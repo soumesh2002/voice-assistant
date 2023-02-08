e37a6dc052f3f25e05a87b4795355be5ae2f8757
soumesh2002 <soumesh6622@gmail.com>
2023-02-09 00:08:18 +0530

    added required packages

---
+++ main.py
@@ -0,0 +1,22 @@
+"""
+Author: Soumesh Khuntia (soumesh2002)
+
+Description: 
+    A simple voice assistant build using Python for a personal project.
+"""
+
+# Importing the packages
+import pyttsx3 as tt
+import speech_recognition as sr
+import requests
+import time
+import os
+
+# engine - (init)
+engine = tt.init()
+
+# main function
+
+
+def main():
+    pass
